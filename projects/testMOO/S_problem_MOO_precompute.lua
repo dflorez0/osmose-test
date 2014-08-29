f40c79bb8aa7ee36ee9ea31e3430e990870b9c71
Samira Fazlollahi <samira.fazlollahi@epfl.ch>
2014-08-29 05:18:53 +0200

    Dakota multi objective optimization test case

---
+++ projects/testMOO/S_problem_MOO_precompute.lua
@@ -0,0 +1,21 @@
+--[[------------------------------------------------------
+
+  #S_problem_MOO_precompute
+  PreCompute function for dakota multi-objective test case
+
+  10.08.2014
+
+  @author: Samira Fazlollahi (samira.fazlollahi@a3.epfl.ch)
+
+  @copyright IPESE
+
+--]]------------------------------------------------------
+
+function S_problem_MOO_precompute(args)
+	local value1 = ( args['x1']*14)
+	setTag('FmaxHotUtility1',value1)
+  local value2 = ( args['x2']*14)
+	setTag('FmaxHotUtility2',value2)
+  
+	solve()
+end
