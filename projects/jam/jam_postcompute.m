3e5971a5f31c65ec66dc0649d913adc82fb95dd6
Renaud Kern <renaud.kern@gmail.com>
2014-11-03 10:52:07 +0100

    update matlab exemple

--- projects/jam/jam_postcompute.m
+++ projects/jam/jam_postcompute.m
@@ -3,7 +3,7 @@ disp('Run in Matlab');
 u = getUnit('CipUnit');
 s = getStream('fresh_water');
 t = getTag('return_temp');
-v = setTag('return_temp', 100);
+v = setTag('return_temp', 150);
 
 disp(['Unit Name: ', u.name]);
 disp(['Stream Name: ', s.name]);
