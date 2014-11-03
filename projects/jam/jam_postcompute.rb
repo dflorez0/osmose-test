daeff31a99992c190536183c2f54da92022affc0
Renaud Kern <renaud.kern@gmail.com>
2014-11-03 10:48:21 +0100

    create example with ruby bridge

---
+++ projects/jam/jam_postcompute.rb
@@ -0,0 +1,8 @@
+def jam_postcompute()
+	puts 'JAM POSTCOMPUTE'
+
+	unit=getUnit('CipUnit')
+	puts unit['name']
+
+	setTag('return_temp', 150)
+end
---
+++ projects/jam/jam_ruby.rb
@@ -0,0 +1,36 @@
+--[[------------------------------------------------------
+
+  # jam MER
+
+  This is an example of a jam factory with MER.
+
+--]]------------------------------------------------------
+
+
+local osmose = require 'lib.osmose'
+
+local project = osmose.Project('LuaJam', 'MER')
+
+project:load(
+	{cip = "ET.Cip"},
+  {utilities = "ET.generic_utilities"},
+	{cm1 = "ET.CookingMixing"},
+	{cm2 = "ET.CookingMixing", with = 'CM2_inputs.csv'}
+)
+
+-- operationalCosts to be used only when objective is YearlyOperatingCost
+--project.operationalCosts={cost_elec_in=2, cost_elec_out=5, op_time=8600}
+
+osmose.Eiampl(project)
+
+project:compute('jam_postcompute.rb')
+
+osmose.Glpk(project)
+
+osmose.Graph(project)
+
+
+
+
+
+
