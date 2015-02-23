0e05399e53581a81d99bfff7421a2ab3f8bec87d
Renaud Kern <renaud.kern@gmail.com>
2015-02-23 15:38:01 +0100

    udpate jam_location.lua with YearlyOperatingCost

--- projects/jam/jam_location.lua
+++ projects/jam/jam_location.lua
@@ -9,22 +9,24 @@
 
 local osmose = require 'lib.osmose'
 
-local project = osmose.Project('LuaJam1', 'MER')
+local project = osmose.Project('JamLoc', 'YearlyOperatingCost')
 
 
 project.clusters = {e1={'L1'}, e2={'L2'}, e3={'L1','L2','L3'}}
 
 
-project.operationalCosts={cost_elec_in=2, cost_elec_out=5, op_time=8600}
+project.operationalCosts={cost_elec_in = 17.19, cost_elec_out = 16.9, op_time=8000.0}
 
 
 project:load(
 	{cip = "ET.Cip", locations={'L1','L2','L3'}},
   {utilities = "ET.generic_utilities", locations={'L1','L2','L3'}},
 	{cm1 = "ET.CookingMixing", locations={'L1','L2','L3'}},
-	{cm2 = "ET.CookingMixing", locations={'L1','L2'},  with = 'CM2_inputs.csv', },
+	{cm2 = "ET.CookingMixing", locations={'L2'},  with = 'CM2_inputs.csv', },
 	{cm3 = "ET.CookingMixing", locations={'L3'}}
 )
+
+
 
 project:solve({graph=false, clusters={'e1','e2'}} )
 -- project:solve({graph=false},clusters=>{'e3'} )
