281455950ffd35d59a0a8c9ece52c54c0d1dbb36
Renaud Kern <renaud.kern@gmail.com>
2015-03-31 18:08:28 +0200

    add jam_ecoinvent exemple

---
+++ projects/jam/jam_ecoinvent.lua
@@ -0,0 +1,40 @@
+--[[------------------------------------------------------
+
+  # jam MER
+
+  This is an example of a jam factory with MER.
+
+--]]------------------------------------------------------
+
+
+local osmose = require 'osmose'
+
+local project = osmose.Project('LuaJam', 'OperatingCost')
+
+project.ecoinvent = {
+        impactMethodName = 'CML 2001', -- USEtox
+        impactCategoryName = 'photochemical oxidation (summer smog)', -- human toxicity
+        name = 'EBIR', -- total
+    }
+
+
+project:load(
+	{cip = "ET.Cip_ecoinvent"},
+  {utilities = "ET.generic_utilities"},
+	{cm1 = "ET.CookingMixing"}
+	--{cm2 = "ET.CookingMixing", with = 'CM2_inputs.csv'}
+)
+
+-- operationalCosts to be used only when objective is YearlyOperatingCost
+project.operationalCosts={cost_elec_in=2, cost_elec_out=5, op_time=8600}
+
+--osmose.Eiampl(project)
+
+-- osmose.Glpk(oneRun)
+
+-- osmose.Graph(oneRun)
+
+project:solve() --{graph=false}
+
+project:compute('jam_postcompute.lua')
+
