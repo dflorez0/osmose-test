b9234996c5155a56d9d6014dd63c09fc266315df
nsc <nils.schueler@epfl.ch>
2015-07-20 09:53:45 +0200

    update test costing frontend to fit renamed files

--- projects/SFA_test_costing.lua
+++ projects/SFA_test_costing.lua
@@ -11,13 +11,13 @@
 
 local osmose = require 'lib.osmose'
 
-local project = osmose.Project('S_Problem_mass2_qt_cost_Time', 'Impact')
+local project = osmose.Project('SFA_Problem_mass2_qt_cost_Time', 'Impact')
 
 --project.operationalCosts = {cost_elec_in = 17.19, cost_elec_out = 16.9, op_time= 2000.0}
 project.operationalCosts = 'op_costs.csv'
 
 project:load(
-	{cip = "S_Problem_mass2_qt_cost_Time", with = 'mts_data.csv'}
+	{cip = "SFA_Problem_mass2_qt_cost_Time", with = 'mts_data.csv'}
 )
 
 project:periode(1):time(3)
