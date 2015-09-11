5cac126deb201d32d13b5421ef02288138770c35
mkermani <maziar.kermani@epfl.ch>
2015-09-11 14:11:49 +0200

    Removing obsolete test cases

--- projects/testMOO/S_problem_MOO_postcompute1.lua
+++
@@ -1,9 +0,0 @@
-function S_problem_MOO_postcompute1(args)
-	local results = getResults()
-
-	local f1 = results.impact[1] or 10000
-	local f2 = results.opcost[1] or 10000
-
-	return {f1,f2}
-end
-
--- projects/testMOO/S_problem_MOO_postcompute2.lua
+++
@@ -1,8 +0,0 @@
-function S_problem_MOO_postcompute2(args)
-
-	local f1 = getResults().impact[1]
-	--local f2 = getResults().opcost[1]
-
-	return f1
-end
-
