70ab4beeb1df64f859bfb0fd555e705518e6c1e2
nsc <nils.schueler@epfl.ch>
2015-10-29 09:20:46 +0000

    add example for how to define parameters/variables/equations for GLPK in Osmose

---
+++ ET/TestGLPK/generic_utilities_with_addParameterData.lua
@@ -0,0 +1,46 @@
+#!/usr/bin/lua
+--[[------------------------------------------------------
+
+  # Derived from generic_utilities
+
+-- example for how to define parameters/variables/equations for GLPK in Osmose
+
+--]]------------------------------------------------------
+
+local osmose = require 'osmose'
+local lib = osmose.Model 'generic_utilities'
+
+-- ## Inlet Parameters  (isVIT = 1)
+lib.inputs = { }
+
+-- ## Result (isVIT = 2)
+lib.outputs = {  }
+
+lib:addEquations{
+  Distance_Constraint = { statement = "subject to Distance_Constraint{t in Time}: \n\t Total_Distance[t] = sum {u in Units} Units_Distance[u,t];", param = {}, addToProblem=1, type ='ampl'} 
+}
+
+lib:addVariables {
+  Total_Distance = {indexedOver = {'Time'}, constraints = '>= 0'}, 
+}
+
+lib:addParameters {
+  Units_Distance = {indexedOver = {'Unit', 'Time'}, type = 'integer', defaultValue = 1, constraints = '>= 0, < 10000'}, 
+}
+
+lib:addParameterData {
+  Units_Distance = {value = 2, indexedOver = {'Units','Time'}, unitName = 'Boiler'},
+}
+
+lib:addUnit("Boiler", {type = 'Utility'})
+lib["Boiler"].Cost2 = 10
+lib["Boiler"]:addStreams({
+  boilingSteam = {120, 100, 119, 0,5}
+  })
+
+lib:addUnit("Cooling", {type = 'Utility'})
+lib["Cooling"]:addStreams({
+  coolingSteam = {3,0,30,100,3}
+  })
+
+return lib
---
+++ projects/GLPK/addParameterData_frontend.lua
@@ -0,0 +1,21 @@
+--- Osmose2GLPK
+-- Frontend template
+-- example for how to define parameters/variables/equations for GLPK in Osmose
+-- @author Nils Schueler 
+-- @release 28.10.2015
+
+local osmose = require 'lib.osmose'
+
+local project = osmose.Project('Test_Osmose2GLPK', 'OperatingCost', 'testing')
+
+project.operationalCosts = {cost_elec_in = 17.19, cost_elec_out = 16.9, op_time=8000}
+
+project:load( 
+  {utilities = 'TestGLPK/generic_utilities_with_addParameterData'},
+  {process = 'Cip'}
+)
+	
+project:solve({graph={format = 'jpg'}}) 
+
+-- check now the eiampl.run file (the lines before the objective is defined) 
+-- and the eiampldata.in file (last lines) to see the added variables/parameters  
